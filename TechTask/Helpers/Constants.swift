fc7ce154bcde9da5a1567fe4e0ea16a065027800
Iniyarajan <iniyarajan.iosdev@gmail.com>
2020-11-05 10:00:01 +0530

    #Added Helper group #Added Constants class

---
+++ TechTask/Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TechTask
+//
+//  Created by MAC on 03/07/19.
+//  Copyright © 2020 MAC. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let ABOUT_API = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
